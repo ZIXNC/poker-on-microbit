cc71441fc420a1d9714e19fdb38c33e61608449a
ZIXNC <testingcomact@gmail.com>
2026-06-10 10:23:52 +0700

    Three Of Kind Complete

--- main.py
+++ main.py
@@ -12,29 +12,30 @@ def on_button_pressed_ab():
     basic.show_number(SPADES)
     basic.clear_screen()
     basic.pause(1000)
-    if CLUBS == SPADES or DIAMONDS == HEARTS or (CLUBS == DIAMONDS or CLUBS == HEARTS) or (DIAMONDS == SPADES or HEARTS == SPADES):
-        basic.show_string("ONEPAIR")
+    
+    if (CLUBS == SPADES and SPADES == DIAMONDS) or (CLUBS == SPADES and SPADES == HEARTS) or (CLUBS == DIAMONDS and DIAMONDS == HEARTS) or (SPADES == DIAMONDS and DIAMONDS == HEARTS):
+        basic.show_string("THREE OF A KIND")
         basic.clear_screen()
         basic.pause(1000)
-    elif CLUBS == SPADES and DIAMONDS == HEARTS or CLUBS == DIAMONDS and HEARTS == SPADES or CLUBS == HEARTS and DIAMONDS == SPADES:
+    elif (CLUBS == SPADES and DIAMONDS == HEARTS) or (CLUBS == DIAMONDS and HEARTS == SPADES) or (CLUBS == HEARTS and DIAMONDS == SPADES):
         basic.show_string("TWOPAIR")
         basic.clear_screen()
         basic.pause(1000)
-    elif SPADES == DIAMONDS and (DIAMONDS == HEARTS and SPADES == HEARTS) or (DIAMONDS == HEARTS and (DIAMONDS == CLUBS and HEARTS == CLUBS) or (HEARTS == CLUBS and (HEARTS == SPADES and CLUBS == SPADES) or CLUBS == SPADES and (CLUBS == DIAMONDS and SPADES == DIAMONDS))):
-        basic.show_string("THREE OF KIND ")
+    elif CLUBS == SPADES or DIAMONDS == HEARTS or CLUBS == DIAMONDS or CLUBS == HEARTS or DIAMONDS == SPADES or HEARTS == SPADES:
+        basic.show_string("ONEPAIR")
         basic.clear_screen()
         basic.pause(1000)
-    elif False:
-        basic.clear_screen()
     else:
-        pass
+        basic.show_string("NO PAIR")
+        basic.clear_screen()
+
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 SPADES = 0
 CLUBS = 0
 DIAMONDS = 0
 HEARTS = 0
-HEARTS = randint(1, 3)
-DIAMONDS = randint(1, 3)
-CLUBS = randint(1, 3)
-SPADES = randint(1, 3)
+HEARTS = randint(1, 5)
+DIAMONDS = randint(1, 5)
+CLUBS = randint(1, 5)
+SPADES = randint(1, 5)
--- main.ts
+++ main.ts
@@ -12,29 +12,28 @@ input.onButtonPressed(Button.AB, function () {
     basic.showNumber(SPADES)
     basic.clearScreen()
     basic.pause(1000)
-    if (CLUBS == SPADES || DIAMONDS == HEARTS || (CLUBS == DIAMONDS || CLUBS == HEARTS) || (DIAMONDS == SPADES || HEARTS == SPADES)) {
-        basic.showString("ONEPAIR")
+    if (CLUBS == SPADES && SPADES == DIAMONDS || CLUBS == SPADES && SPADES == HEARTS || CLUBS == DIAMONDS && DIAMONDS == HEARTS || SPADES == DIAMONDS && DIAMONDS == HEARTS) {
+        basic.showString("THREE OF A KIND")
         basic.clearScreen()
         basic.pause(1000)
     } else if (CLUBS == SPADES && DIAMONDS == HEARTS || CLUBS == DIAMONDS && HEARTS == SPADES || CLUBS == HEARTS && DIAMONDS == SPADES) {
         basic.showString("TWOPAIR")
         basic.clearScreen()
         basic.pause(1000)
-    } else if (SPADES == DIAMONDS && (DIAMONDS == HEARTS && SPADES == HEARTS) || (DIAMONDS == HEARTS && (DIAMONDS == CLUBS && HEARTS == CLUBS) || (HEARTS == CLUBS && (HEARTS == SPADES && CLUBS == SPADES) || CLUBS == SPADES && (CLUBS == DIAMONDS && SPADES == DIAMONDS)))) {
-        basic.showString("THREE OF KIND ")
+    } else if (CLUBS == SPADES || DIAMONDS == HEARTS || CLUBS == DIAMONDS || CLUBS == HEARTS || DIAMONDS == SPADES || HEARTS == SPADES) {
+        basic.showString("ONEPAIR")
         basic.clearScreen()
         basic.pause(1000)
-    } else if (false) {
-        basic.clearScreen()
     } else {
-    	
+        basic.showString("NO PAIR")
+        basic.clearScreen()
     }
 })
 let SPADES = 0
-let CLUBS = 0
 let DIAMONDS = 0
+let CLUBS = 0
 let HEARTS = 0
-HEARTS = randint(1, 3)
-DIAMONDS = randint(1, 3)
-CLUBS = randint(1, 3)
-SPADES = randint(1, 3)
+HEARTS = randint(1, 12)
+CLUBS = randint(1, 12)
+DIAMONDS = randint(2, 12)
+SPADES = randint(2, 12)
